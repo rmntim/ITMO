6939b233267c599c0820589d54c0f02bb0563314
Roman Timoshkin <rmntim@yandex.ru>
2023-11-06 13:51:25 +0300

    fix (#2): removed useless fields from `Action` subclasses and deleted `getDescription` method from `Action` since it's not used

--- Programming/Labwork3/src/main/java/com/rmntim/models/actions/GoAction.java
+++ Programming/Labwork3/src/main/java/com/rmntim/models/actions/GoAction.java
@@ -1,31 +1,7 @@
 package com.rmntim.models.actions;
 
-import java.util.Objects;
-
 public class GoAction extends Action {
-    private final String destination;
-
     public GoAction(String destination) {
-        super("отправляться");
-        this.destination = destination;
-    }
-
-    @Override
-    public String toString() {
-        return getDescription() + " " + destination;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        GoAction goAction = (GoAction) o;
-        return Objects.equals(destination, goAction.destination);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), destination);
+        super("отправляться " + destination);
     }
 }
