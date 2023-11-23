a14587797d27469b3e49b8a81760e780aad0b215
Roman Timoshkin <rmntim@yandex.ru>
2023-11-23 16:15:48 +0300

    misc: finished programming 4th lab

--- Programming/Labwork4/src/main/java/com/rmntim/exceptions/EmptyStoryException.java
+++ Programming/Labwork4/src/main/java/com/rmntim/exceptions/EmptyStoryException.java
@@ -1,6 +1,6 @@
 package com.rmntim.exceptions;
 
-public class EmptyStoryException extends Throwable {
+public class EmptyStoryException extends Exception {
     public EmptyStoryException() {
         super("Story is empty");
     }
