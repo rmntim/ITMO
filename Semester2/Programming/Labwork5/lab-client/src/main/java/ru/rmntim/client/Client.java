256fe909c08231613283147444d947695ed9e67e
Roman Timoshkin <rmntim@yandex.ru>
2024-02-14 14:50:09 +0300

    programming(5): new beginning

---
+++ Semester2/Programming/Labwork5/lab-client/src/main/java/ru/rmntim/client/Client.java
@@ -0,0 +1,10 @@
+package ru.rmntim.client;
+
+public final class Client {
+    private Client() {
+        throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Semester2/Programming/Labwork5/lab-server/src/main/java/ru/rmntim/server/Server.java
@@ -0,0 +1,12 @@
+package ru.rmntim.server;
+
+public final class Server {
+
+    private Server() {
+        throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
