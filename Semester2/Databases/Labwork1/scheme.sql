15937ac5839530553a9838ea262a0307c1d3eae1
Roman Timoshkin <rmntim@yandex.ru>
2024-02-20 16:11:46 +0300

    db(1): fixed schema script

--- Semester2/Databases/Labwork1/scheme.sql
+++ Semester2/Databases/Labwork1/scheme.sql
@@ -8,31 +8,31 @@ CREATE TYPE contact_reason AS ENUM (
 CREATE TABLE people (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  sex sex NOT NULL,
+  sex sex NOT NULL
 );
 
 CREATE TABLE council_members (
   id SERIAL PRIMARY KEY,
-  person_id INT NOT NULL REFERENCES people(id),
+  person_id INT NOT NULL REFERENCES people
 );
 
 CREATE TABLE apprentices (
   id SERIAL PRIMARY KEY,
-  person_id INT NOT NULL REFERENCES people(id),
-  master_id INT NOT NULL REFERENCES council_members(id)
+  person_id INT NOT NULL REFERENCES people,
+  master_id INT NOT NULL REFERENCES council_members
 );
 
 CREATE TABLE contacts (
   id SERIAL PRIMARY KEY,
   reason contact_reason NOT NULL,
-  made_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
-  subject_id INT NOT NULL REFERENCES people(id),
+  made_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  subject_id INT NOT NULL REFERENCES people
 );
 
 CREATE TABLE people_contacts (
-  contacted_person_id  INT NOT NULL REFERENCES people(id),
-  contact_id INT NOT NULL REFERENCES contacts(id),
-  CONSTRAINT people_contact_id PRIMARY KEY (contacted_person_id, contact_id),
+  contacted_person_id  INT NOT NULL REFERENCES people,
+  contact_id INT NOT NULL REFERENCES contacts,
+  CONSTRAINT people_contact_id PRIMARY KEY (contacted_person_id, contact_id)
 );
 
 INSERT INTO people (name, sex)
@@ -49,7 +49,7 @@ INSERT INTO apprentices (person_id, master_id)
 VALUES (2, 1);
 
 INSERT INTO contacts (reason, subject_id)
-VALUES ('возбудить интересь', 1);
+VALUES ('возбудить интерес', 1);
 
 INSERT INTO people_contacts (contacted_person_id, contact_id)
 VALUES (3, 1), (4, 1), (5, 1);
