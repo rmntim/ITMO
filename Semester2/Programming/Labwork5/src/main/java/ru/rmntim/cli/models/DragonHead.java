7c50c3e203dc13780120a087b413045654e7e7f2
Roman Timoshkin <rmntim@yandex.ru>
2024-02-27 18:30:39 +0300

    programming(5): convert classes to records

--- Semester2/Programming/Labwork5/src/main/java/ru/rmntim/cli/models/DragonHead.java
+++ Semester2/Programming/Labwork5/src/main/java/ru/rmntim/cli/models/DragonHead.java
@@ -1,25 +1,4 @@
 package ru.rmntim.cli.models;
 
-public class DragonHead {
-    private Double eyesCount; //Поле не может быть null
-
-    public DragonHead(Double eyesCount) {
-        setEyesCount(eyesCount);
-    }
-
-    public Double getEyesCount() {
-        return eyesCount;
-    }
-
-    public void setEyesCount(Double eyesCount) {
-        if (eyesCount == null) {
-            throw new IllegalArgumentException("Eyes count cannot be null");
-        }
-        this.eyesCount = eyesCount;
-    }
-
-    @Override
-    public String toString() {
-        return "DragonHead{" + "eyesCount=" + eyesCount + '}';
-    }
+public record DragonHead(Double eyesCount) {
 }
