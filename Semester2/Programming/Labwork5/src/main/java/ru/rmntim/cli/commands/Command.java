1af05c4e7c2af03afa2477bd4975331ef672f7a8
Roman Timoshkin <rmntim@yandex.ru>
2024-02-29 19:48:30 +0300

    programming(5): add command javadoc

--- Semester2/Programming/Labwork5/src/main/java/ru/rmntim/cli/commands/Command.java
+++ Semester2/Programming/Labwork5/src/main/java/ru/rmntim/cli/commands/Command.java
@@ -10,13 +10,33 @@ public abstract class Command {
     private final String description;
     private final List<String> parameters;
 
+    /**
+     * @param name        command name
+     * @param description command description
+     * @throws NullPointerException     if name or description or is null
+     * @throws IllegalArgumentException if name or description is empty
+     */
     public Command(String name, String description) {
-        this.name = name;
-        this.description = description;
-        this.parameters = List.of();
+        this(name, description, List.of());
     }
 
+    /**
+     * @param name        command name
+     * @param description command description shown in help
+     * @param parameters  inline command parameters
+     * @throws NullPointerException     if name, description or parameters are null
+     * @throws IllegalArgumentException if name or description is empty
+     */
     public Command(String name, String description, List<String> parameters) {
+        if (name == null || description == null || parameters == null) {
+            throw new NullPointerException();
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name can't be empty");
+        }
+        if (description.isBlank()) {
+            throw new IllegalArgumentException("Description can't be empty");
+        }
         this.name = name;
         this.description = description;
         this.parameters = parameters;
@@ -35,8 +55,8 @@ public abstract class Command {
     }
 
     /**
-     * @param arguments list of arguments to the command, not including the command name.
-     * @param context
+     * @param arguments list of arguments to the command, not including the command name
+     * @param context   execution context
      * @throws ru.rmntim.cli.exceptions.BadCommandArgumentsException if argument list is not valid
      */
     public abstract void execute(List<String> arguments, ExecutionContext context);
