59a3e99c076383f1968fee1f1699e72c7beb0cf9
Roman Timoshkin <rmntim@yandex.ru>
2024-03-11 15:35:30 +0300

    bpa(4): add code for program

--- Semester2/BasicsOfProfessionalActivity/Labwork4/main.asm
+++ Semester2/BasicsOfProfessionalActivity/Labwork4/main.asm
@@ -1,1 +1,50 @@
-; TODO
+org 0x1ef
+
+cla
+st R
+ld X
+push
+call $func
+pop
+inc
+sub R
+st R
+ld Y
+dec
+push
+call $func
+pop
+sub R
+st R
+ld Z
+inc
+push
+call $func
+pop
+inc
+add R
+st R
+hlt
+
+Z: word 0x0064
+Y: word 0xFFFB
+X: word 0xBEEF
+R: word 0xffd3
+
+;; subroutine
+org 0x6fe
+func:
+      ld &1
+      bns calc
+      cmp N
+      beq dflt
+      blt dflt
+calc: asl
+      sub M
+      jump fini
+dflt: ld N
+fini: st &1
+      ret
+
+N: word 2003
+M: word 43
