680074b33e7b2260462b0652f10d9c75ae149e8d
Roman Timoshkin <rmntim@yandex.ru>
2024-04-04 18:44:38 +0300

    programming(6): fix checkstyle error

--- Semester2/Programming/Labwork6/lab-server/src/main/java/ru/rmntim/server/lib/CollectionManager.java
+++ Semester2/Programming/Labwork6/lab-server/src/main/java/ru/rmntim/server/lib/CollectionManager.java
@@ -69,6 +69,7 @@ public class CollectionManager {
         try {
             storageManager.writeCollection(collection);
         } catch (IOException ignored) {
+            // ignore
         }
     }
 }
