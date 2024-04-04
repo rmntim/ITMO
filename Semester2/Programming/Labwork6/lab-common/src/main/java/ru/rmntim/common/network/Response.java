1aed04cdec406ee4455682ec3a075c277b7d785d
Roman Timoshkin <rmntim@yandex.ru>
2024-04-04 17:02:03 +0300

    programming(6): better error indication

--- Semester2/Programming/Labwork6/lab-common/src/main/java/ru/rmntim/common/network/Response.java
+++ Semester2/Programming/Labwork6/lab-common/src/main/java/ru/rmntim/common/network/Response.java
@@ -2,8 +2,13 @@ package ru.rmntim.common.network;
 
 import java.io.Serializable;
 
-public record Response(String message, boolean isError) implements Serializable {
+public record Response(Status status, String message) implements Serializable {
+    public enum Status {
+        OK,
+        ERROR,
+    }
+
     public Response(String message) {
-        this(message, false);
+        this(Status.OK, message);
     }
 }
