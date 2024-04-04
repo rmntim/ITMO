269c1dd1ff21cfd5dc6ce81eb8032b213d5a3ff6
Roman Timoshkin <rmntim@yandex.ru>
2024-04-04 17:24:21 +0300

    programming(6): create basic client

--- Semester2/Programming/Labwork6/lab-client/src/main/java/ru/rmntim/client/Client.java
+++ Semester2/Programming/Labwork6/lab-client/src/main/java/ru/rmntim/client/Client.java
@@ -1,5 +1,14 @@
 package ru.rmntim.client;
 
+import org.apache.commons.lang3.SerializationUtils;
+import ru.rmntim.client.network.UDPClient;
+import ru.rmntim.common.commands.Info;
+import ru.rmntim.common.network.Response;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 public final class Client {
     private static final int PORT = 1337;
 
@@ -7,6 +16,14 @@ public final class Client {
         throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        var command = new Info();
+        var commandBytes = SerializationUtils.serialize(command);
+
+        var client = new UDPClient(new InetSocketAddress(InetAddress.getLocalHost(), PORT));
+        var responseBytes = client.sendThenRecieve(commandBytes);
+        var response = (Response) SerializationUtils.deserialize(responseBytes);
+        System.out.println("Status: " + response.status());
+        System.out.println("Message: \n---\n" + response.message() + "\n---");
     }
 }
--- Semester2/Programming/Labwork6/lab-client/src/main/java/ru/rmntim/client/network/UDPClient.java
+++ Semester2/Programming/Labwork6/lab-client/src/main/java/ru/rmntim/client/network/UDPClient.java
@@ -1,12 +1,45 @@
 package ru.rmntim.client.network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+
 /**
  * Manages the connection to the server.
  */
 public class UDPClient {
-    private final int port;
+    private static final int PACKET_SIZE = 1024;
+    private final SocketAddress serverAddress;
+    private final DatagramSocket datagramSocket;
 
-    public UDPClient(int port) {
-        this.port = port;
+    /**
+     * Creates new client.
+     *
+     * @param serverAddress the address of the server
+     * @throws SocketException if socket can't be created
+     */
+    public UDPClient(SocketAddress serverAddress) throws SocketException {
+        this.serverAddress = serverAddress;
+        this.datagramSocket = new DatagramSocket();
+    }
+
+    /**
+     * Sends the command to server and waits for a response.
+     *
+     * @param data the data to send
+     * @return the received data
+     * @throws IOException if an I/O error occurs
+     */
+    public byte[] sendThenRecieve(byte[] data) throws IOException {
+        var packet = new DatagramPacket(data, data.length, serverAddress);
+        datagramSocket.send(packet);
+
+        var buffer = new byte[PACKET_SIZE];
+        var serverPacket = new DatagramPacket(buffer, buffer.length);
+        datagramSocket.receive(serverPacket);
+
+        return serverPacket.getData();
     }
 }
