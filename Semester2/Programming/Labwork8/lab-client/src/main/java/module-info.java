1b7e5a634a3535aefb4290b7fc8ce8eb847a3fe5
Roman Timoshkin <rmntim@yandex.ru>
2024-05-14 15:18:31 +0300

    programming(8): add module info to client

--- Semester2/Programming/Labwork8/lab-client/src/main/java/module-info.java
+++ Semester2/Programming/Labwork8/lab-client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ru.rmntim.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires ru.rmntim.common;
 
 
     opens ru.rmntim.client to javafx.fxml;
---
+++ Semester2/Programming/Labwork8/lab-common/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module ru.rmntim.common {
+    exports ru.rmntim.common.commands;
+    exports ru.rmntim.common.network;
+    exports ru.rmntim.common.validators;
+    exports ru.rmntim.common.models;
+}
