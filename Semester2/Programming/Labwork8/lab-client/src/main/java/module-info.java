01f1e45ffad43b4c108f1d037d0e746d4b6c60a5
Roman Timoshkin <rmntim@yandex.ru>
2024-05-14 15:24:16 +0300

    programming(8): add udp client

--- Semester2/Programming/Labwork8/lab-client/src/main/java/module-info.java
+++ Semester2/Programming/Labwork8/lab-client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ru.rmntim.client {
     requires javafx.controls;
     requires javafx.fxml;
     requires ru.rmntim.common;
+    requires org.apache.commons.lang3;
 
 
     opens ru.rmntim.client to javafx.fxml;
