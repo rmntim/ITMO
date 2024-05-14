a44e15227f54e09ab2c24d98eefbbae58c99c857
Roman Timoshkin <rmntim@yandex.ru>
2024-05-14 15:05:27 +0300

    programming(8): use vars instead of types

--- Semester2/Programming/Labwork8/lab-client/src/main/java/ru/rmntim/client/HelloApplication.java
+++ Semester2/Programming/Labwork8/lab-client/src/main/java/ru/rmntim/client/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        var fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        var scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
