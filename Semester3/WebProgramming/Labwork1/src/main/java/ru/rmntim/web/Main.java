8ba3c1b90b14a06fbd7741d2d2e540585681f67f
Roman Timoshkin <rmntim@yandex.ru>
2024-09-07 18:53:04 +0300

    web(1): fix last things

--- Semester3/WebProgramming/Labwork1/src/main/java/ru/rmntim/web/Main.java
+++ Semester3/WebProgramming/Labwork1/src/main/java/ru/rmntim/web/Main.java
@@ -6,11 +6,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
-    private record Params(int x, float y, float r) {
+    private static class ValidationError extends Exception {
+        public ValidationError(String message) {
+            super(message);
+        }
     }
 
     public static void main(String[] args) {
@@ -21,7 +25,7 @@ public class Main {
             var uriString = System.getProperties().getProperty("REQUEST_URI");
 
             if (uriString == null || uriString.isEmpty()) {
-                System.out.println("No request URI provided");
+                System.err.println("No request URI provided");
                 continue;
             }
 
@@ -29,7 +33,7 @@ public class Main {
                 var uri = new URI(uriString);
                 var params = splitQuery(uri);
                 var validParams = validateParams(params);
-                var result = calculate(validParams);
+                var result = calculate(validParams.get("x"), validParams.get("y"), validParams.get("r"));
 
                 if (result) {
                     System.out.println("{ \"result\": \"hit\" }");
@@ -37,17 +41,62 @@ public class Main {
                     System.out.println("{ \"result\": \"miss\" }");
                 }
             } catch (URISyntaxException e) {
-                System.out.println("Invalid request URI: " + uriString);
+                System.err.println("Invalid request URI: " + uriString);
+            } catch (ValidationError e) {
+                System.err.println(e.getMessage());
             }
         }
     }
 
-    private static Params validateParams(Map<String, String> params) {
-        var x = Integer.parseInt(params.get("x"));
-        var y = Float.parseFloat(params.get("y"));
-        var r = Float.parseFloat(params.get("r"));
+    private static Map<String, Float> validateParams(Map<String, String> params) throws ValidationError {
+        var result = new HashMap<String, Float>();
 
-        return new Params(x, y, r);
+        var x = params.get("x");
+        if (x == null || x.isEmpty()) {
+            throw new ValidationError("x is invalid");
+        }
+
+        try {
+            var xx = Integer.parseInt(x);
+            if (xx < -3 || xx > 5) {
+                throw new ValidationError("x has forbidden value");
+            }
+            result.put("x", (float) xx);
+        } catch (NumberFormatException e) {
+            throw new ValidationError("x is not a number");
+        }
+
+        var y = params.get("y");
+        if (y == null || y.isEmpty()) {
+            throw new ValidationError("y is invalid");
+        }
+
+        try {
+            var yy = Float.parseFloat(y);
+            if (yy < -3 || yy > 5) {
+                throw new ValidationError("y has forbidden value");
+            }
+            result.put("y", yy);
+        } catch (NumberFormatException e) {
+            throw new ValidationError("y is not a number");
+        }
+
+        var r = params.get("r");
+        if (r == null || r.isEmpty()) {
+            throw new ValidationError("r is invalid");
+        }
+
+        try {
+            var rr = Float.parseFloat(r);
+            if (rr < 1 || rr > 3) {
+                throw new ValidationError("r has forbidden value");
+            }
+            result.put("r", rr);
+        } catch (NumberFormatException e) {
+            throw new ValidationError("r is not a number");
+        }
+
+        return result;
     }
 
     private static Map<String, String> splitQuery(URI uri) {
@@ -61,7 +110,25 @@ public class Main {
         return query_pairs;
     }
 
-    private static boolean calculate(Params params) {
-        return false;
+    private static boolean calculate(float x, float y, float r) {
+        if (x > 0 && y > 0) {
+            return false;
+        }
+        if (x > 0 && y < 0) {
+            if ((x * x + y * y) > (r / 2) * (r / 2)) {
+                return false;
+            }
+        }
+        if (x < 0 && y < 0) {
+            if ((x / 2 + y) < -r / 2) {
+                return false;
+            }
+        }
+        if (x < 0 && y > 0) {
+            if (x < -r / 2 || y > r) {
+                return false;
+            }
+        }
+        return true;
     }
 }
