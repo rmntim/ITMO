fc5d50c0aa66333dfe399b870a9b25c5b45d3493
Roman Timoshkin <rmntim@yandex.ru>
2024-09-08 11:02:40 +0300

    web(1): remove local dev artifacts

--- Semester3/WebProgramming/Labwork1/src/main/java/ru/rmntim/web/Main.java
+++ Semester3/WebProgramming/Labwork1/src/main/java/ru/rmntim/web/Main.java
@@ -10,27 +10,25 @@ public class Main {
             Content-Type: application/json
             Content-Length: %d
             
-            
             %s
-            
             """;
     private static final String HTTP_ERROR = """
             HTTP/1.1 400 Bad Request
             Content-Type: application/json
             Content-Length: %d
             
-            
             %s
-            
             """;
     private static final String RESULT_JSON = """
             {
                 "result": %b
-            }""";
+            }
+            """;
     private static final String ERROR_JSON = """
             {
                 "reason": "%s"
-            }""";
+            }
+            """;
 
 
     public static void main(String[] args) {
@@ -43,11 +41,11 @@ public class Main {
                 var result = calculate(params.getX(), params.getY(), params.getR());
 
                 var json = String.format(RESULT_JSON, result);
-                var response = String.format(HTTP_RESPONSE, json.getBytes(StandardCharsets.UTF_8).length, json);
+                var response = String.format(HTTP_RESPONSE, json.getBytes(StandardCharsets.UTF_8).length + 2, json);
                 System.out.println(response);
             } catch (ValidationException e) {
                 var json = String.format(ERROR_JSON, e.getMessage());
-                var response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length, json);
+                var response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length + 2, json);
                 System.out.println(response);
             }
         }
