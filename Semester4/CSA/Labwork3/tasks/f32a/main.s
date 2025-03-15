af5243977d638b4256624ae5d54a5c9643e9f2c1
Roman Timoshkin <rmntim@icloud.com>
2025-03-15 21:30:47 +0300

    csa(3): publish f32a

--- Semester4/CSA/Labwork3/tasks/f32a/main.s
+++ Semester4/CSA/Labwork3/tasks/f32a/main.s
@@ -1,28 +1,54 @@
     .data
-
+len:             .byte  0
+buf:             .byte  '_______________________________'
+padding:         .byte  '___'
 input_addr:      .word  0x80
 output_addr:     .word  0x84
+mask:            .byte  0,'___'
+bullshit:        .word  0xCCCCCCCC
 
     .text
 
+.org 0x90
 _start:
-    @p output_addr b!        \ b = output_addr
-    @p input_addr a!         \ a = input_addr
+    @p input_addr b!         \ b = input_addr
+    lit buf a!               \ a = buf
 
-    @                        \ stack = len
 while:
-    dup                      \ stack = len-1 , len-1
-    if end                   \ if len-1 == 0 goto end
-
-    @ lit 255 and dup        \ stack = len-1 , char, char
-    lit -97 +                \ stack = len-1 , char, char-97
-    -if upcase               \ if char-97 >= 0 goto upcase
+    @b lit 255 and dup       \ stack = char&0xff, char&0xff
+    lit -10 + if end         \ if char&0xff-10 == '\n' goto end
+                             \ stack = char&0xff
+    dup lit -97 +            \ stack = char&0xff, char&0xff-97
+    -if upcase               \ if char&0xff-97 >= 0 goto upcase
     out ;                    \ else goto out
 upcase:
-    lit -32 +                \ stack = len-1 , char-32
+    lit -32 +                \ stack = char-32
 out:
-    !b                       \ stack = len-1 \ *output_addr = b
-    lit -1 +                 \ stack = len-2
+    @p mask +                \ stack = masked
+    !+                       \ stack empty \ *buf++ = char-32
+    @p len                   \ stack = len
+    lit 1 +                  \ stack = len+1
+    dup                      \ stack = len+1, len+1
+    !p len                   \ stack = len+1
+    lit 255 and              \ stack = len+1&0xff
+    lit -32 + if err         \ if len+1-32 == 0 goto end
     while ;                  \ goto while
+err:
+    @p bullshit
+    @p output_addr b!
+    !b
+    halt
 end:
+    drop
+    @p output_addr b!
+    lit len a!
+    @+ lit 255 and
+end_while:
+    dup if end_end
+    lit -1 +
+    @+ lit 255 and
+    !b
+    end_while ;
+end_end:
+    drop
     halt
