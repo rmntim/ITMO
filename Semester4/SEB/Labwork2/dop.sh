1d6dc068bed5dce4897a3c12bfe24ecf8bfc82f3
Roman Timoshkin <rmntim@icloud.com>
2025-04-10 17:22:22 +0300

    seb(2): fix doptask

--- Semester4/SEB/Labwork2/dop.sh
+++ Semester4/SEB/Labwork2/dop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -ex
+
+cd repo
+
 git cherry-pick -X ours --allow-empty --empty=keep -m 1 $(seq 63 84 | xargs -L1 -I{} git log --grep "r{}" | grep commit | awk '{print $2}')
 git cherry-pick -X ours --allow-empty --empty=keep -m 1 $(seq 62 41 | xargs -L1 -I{} git log --grep "r{}" | grep commit | awk '{print $2}')
 git cherry-pick -X ours --allow-empty --empty=keep -m 1 $(seq 19 40 | xargs -L1 -I{} git log --grep "r{}" | grep commit | awk '{print $2}')
