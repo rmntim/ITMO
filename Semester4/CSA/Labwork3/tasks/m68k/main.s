642b2ca896bed5646ff55017845db87e6830527b
Roman Timoshkin <rmntim@icloud.com>
2025-06-12 17:22:30 +0300

    csa(3): m68k update

--- Semester4/CSA/Labwork3/tasks/m68k/main.s
+++ Semester4/CSA/Labwork3/tasks/m68k/main.s
@@ -1,27 +1,42 @@
     .data
 
-input_addr: .word 0x80
+input_addr:  .word 0x80
+memory_size: .word 0x1000
 
     .text
 
 _start:
-    movea.l input_addr, A0
-    movea.l (A0), A0
-    move.l (A0), D0
-    move.l 0, D1
-    move.b 31, D2
-    move.l 1, D3
-    move.l 0x80000000, D4
-loop:
-    move.l D0, D5
-    and.l D3, D5
-    beq skip_bit_set
-    or.l D4, D1
-skip_bit_set:
-    lsl.l 1, D3
-    lsr.l 1, D4
-    sub.b 1, D2
-    bpl loop
-done:
-    move.l D1, 4(A0)
+    movea.l memory_size, A7  ; A7 = &memory_size
+    movea.l (A7), A7         ; A7 = memory_size
+
+    movea.l input_addr, A0   ; A0 = &input_addr
+    movea.l (A0), A0         ; A0 = input_addr
+
+    move.l (A0), D0          ; D0 = *input_addr
+
+    jsr reverse_bits
+
+    move.l D0, 4(A0)         ; *(input_addr + 4) = result
     halt
+
+reverse_bits:
+    link A6, -4              ; save fp, decrement sp
+    move.l D0, -4(A6)        ; int orig = D0
+
+    move.l 0, D0             ; result
+    move.b 31, D1            ; counter
+    move.l 1, D2             ; low_mask
+    move.l 0x80000000, D3    ; high_mask
+loop:
+    move.l -4(A6), D4        ; temp
+    and.l D2, D4             ; temp &= low_mask
+    beq skip_bit_set         ; if ^ == 0 goto skip_bit_set
+    or.l D3, D0              ; else result |= high_mask
+skip_bit_set:
+    lsl.l 1, D2              ; low_mask << 1
+    lsr.l 1, D3              ; high_mask >> 1
+    sub.b 1, D1              ; counter--
+    bpl loop                 ; while i >= 0
+done:
+    unlk A6                  ; restore sp, pop fp
+    rts
